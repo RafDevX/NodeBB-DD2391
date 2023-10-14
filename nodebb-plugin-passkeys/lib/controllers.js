3dd1dae9e5947474641a789f17b93fb857b60c45
Rafael Oliveira <rmfseo@kth.se>
2023-10-14 17:40:56 +0200

    style(passkeys): formatting

--- nodebb-plugin-passkeys/lib/controllers.js
+++ nodebb-plugin-passkeys/lib/controllers.js
@@ -19,110 +19,128 @@ const helpers = require.main.require('./src/controllers/helpers');
 
 const Controllers = {};
 
-
 async function getGroups(set) {
-	let groupNames = await groups.getGroups(set, 0, -1);
-	groupNames = groupNames.filter(groupName => groupName && !groups.isPrivilegeGroup(groupName));
+    let groupNames = await groups.getGroups(set, 0, -1);
+    groupNames = groupNames.filter(
+        (groupName) => groupName && !groups.isPrivilegeGroup(groupName)
+    );
 
-	return groupNames.map(groupName => ({
-		name: validator.escape(String(groupName)),
-		value: validator.escape(String(groupName)),
-	}));
+    return groupNames.map((groupName) => ({
+        name: validator.escape(String(groupName)),
+        value: validator.escape(String(groupName)),
+    }));
 }
 
 Controllers.renderChoices = async (req, res) => {
-	const uid = res.locals['passkeys'] || req.uid;
+    const uid = res.locals['passkeys'] || req.uid;
 
-  // TODO ?
-	res.render('2fa-choices', {
-		hasAuthn,
-		hasTotp,
-		hasBackupCodes,
-		next: req.query.next,
-		title: '[[passkeys:title]]',
-	});
+    // TODO ?
+    res.render('2fa-choices', {
+        hasAuthn,
+        hasTotp,
+        hasBackupCodes,
+        next: req.query.next,
+        title: '[[passkeys:title]]',
+    });
 };
 
 Controllers.renderAuthnChallenge = async (req, res, next) => {
-	const uid = res.locals['passkeys'] || req.uid;
-	const single = parseInt(req.query.single, 10) === 1;
+    const uid = res.locals['passkeys'] || req.uid;
+    const single = parseInt(req.query.single, 10) === 1;
 
-	if (req.session.tfa === true && ((req.query.next && !req.query.next.startsWith('/admin')) || !req.session.tfaForce)) {
-		return res.redirect(nconf.get('relative_path') + (req.query.next || '/'));
-	}
+    if (
+        req.session.tfa === true &&
+        ((req.query.next && !req.query.next.startsWith('/admin')) ||
+            !req.session.tfaForce)
+    ) {
+        return res.redirect(
+            nconf.get('relative_path') + (req.query.next || '/')
+        );
+    }
 
-	if (!await parent.hasPasskeys(uid)) {
-		return next();
-	}
+    if (!(await parent.hasPasskeys(uid))) {
+        return next();
+    }
 
-	const keyIds = await parent.getAuthnKeyIds(uid);
-	let authnOptions;
-	if (keyIds.length) {
-		authnOptions = await parent._f2l.assertionOptions();
-		authnOptions.allowCredentials = keyIds.map(keyId => ({
-			id: keyId,
-			type: 'public-key',
-			transports: ['usb', 'ble', 'nfc'],
-		}));
-		authnOptions.challenge = base64url(authnOptions.challenge);
-		req.session.authRequest = authnOptions.challenge;
-	}
+    const keyIds = await parent.getAuthnKeyIds(uid);
+    let authnOptions;
+    if (keyIds.length) {
+        authnOptions = await parent._f2l.assertionOptions();
+        authnOptions.allowCredentials = keyIds.map((keyId) => ({
+            id: keyId,
+            type: 'public-key',
+            transports: ['usb', 'ble', 'nfc'],
+        }));
+        authnOptions.challenge = base64url(authnOptions.challenge);
+        req.session.authRequest = authnOptions.challenge;
+    }
 
-	res.render('login-authn', {
-		single,
-		authnOptions,
-		next: req.query.next,
-	});
+    res.render('login-authn', {
+        single,
+        authnOptions,
+        next: req.query.next,
+    });
 };
 
 Controllers.renderAdminPage = async function (req, res, next) {
-	const groups = await getGroups('groups:createtime');
+    const groups = await getGroups('groups:createtime');
 
-	async.parallel({
-		users: async.apply(parent.getUsers),
-	}, (err, data) => {
-		if (err) {
-			return next(err);
-		}
+    async.parallel(
+        {
+            users: async.apply(parent.getUsers),
+        },
+        (err, data) => {
+            if (err) {
+                return next(err);
+            }
 
-		data.groups = groups;
-		data.title = '[[passkeys:title]]';
-		res.render('admin/plugins/passkeys', data);
-	});
+            data.groups = groups;
+            data.title = '[[passkeys:title]]';
+            res.render('admin/plugins/passkeys', data);
+        }
+    );
 };
 
 Controllers.renderSettings = async (req, res) => {
-	const { username, userslug } = await user.getUserFields(res.locals.uid, ['username', 'userslug']);
-	if (res.locals.uid !== req.user.uid) {
-		return helpers.notAllowed(req, res);
-	}
+    const { username, userslug } = await user.getUserFields(res.locals.uid, [
+        'username',
+        'userslug',
+    ]);
+    if (res.locals.uid !== req.user.uid) {
+        return helpers.notAllowed(req, res);
+    }
 
-	const title = await translator.translate('[[passkeys:title]]');
-	const breadcrumbs = helpers.buildBreadcrumbs([
-		{
-			text: username,
-			url: `/user/${userslug}`,
-		},
-		{
-			text: '[[passkeys:title]]',
-		},
-	]);
+    const title = await translator.translate('[[passkeys:title]]');
+    const breadcrumbs = helpers.buildBreadcrumbs([
+        {
+            text: username,
+            url: `/user/${userslug}`,
+        },
+        {
+            text: '[[passkeys:title]]',
+        },
+    ]);
 
-	let { tfaEnforcedGroups } = await meta.settings.get('passkeys');
-	let forceTfa = false;
+    let { tfaEnforcedGroups } = await meta.settings.get('passkeys');
+    let forceTfa = false;
 
-	tfaEnforcedGroups = JSON.parse(tfaEnforcedGroups || '[]');
-	if (tfaEnforcedGroups.length && (await groups.isMemberOfGroups(req.user.uid, tfaEnforcedGroups)).includes(true)) {
-		forceTfa = true;
-	}
+    tfaEnforcedGroups = JSON.parse(tfaEnforcedGroups || '[]');
+    if (
+        tfaEnforcedGroups.length &&
+        (
+            await groups.isMemberOfGroups(req.user.uid, tfaEnforcedGroups)
+        ).includes(true)
+    ) {
+        forceTfa = true;
+    }
 
-	const hasPasskey = await parent.hasPasskey(req.user.uid);
-	res.render('account/passkeys', {
-		title,
-		breadcrumbs,
-		forceTfa,
-		hasPasskey,
-	});
+    const hasPasskey = await parent.hasPasskey(req.user.uid);
+    res.render('account/passkeys', {
+        title,
+        breadcrumbs,
+        forceTfa,
+        hasPasskey,
+    });
 };
 
 module.exports = Controllers;
--- nodebb-plugin-passkeys/static/lib/settings.js
+++ nodebb-plugin-passkeys/static/lib/settings.js
@@ -1,57 +1,76 @@
 'use strict';
 
-define('forum/account/passkeys', ['api', 'alerts', 'bootbox'], function (api, alerts, bootbox) {
-	var Settings = {};
+define('forum/account/passkeys', ['api', 'alerts', 'bootbox'], function (
+    api,
+    alerts,
+    bootbox
+) {
+    const Settings = {};
 
-	Settings.init = function () {
-		document.querySelector('#content .list-group').addEventListener('click', (e) => {
-			if (!e.target.closest('[data-action]') || Array.from(e.target.classList).includes('text-muted')) {
-				return;
-			}
+    Settings.init = function () {
+        document
+            .querySelector('#content .list-group')
+            .addEventListener('click', (e) => {
+                if (
+                    !e.target.closest('[data-action]') ||
+                    Array.from(e.target.classList).includes('text-muted')
+                ) {
+                    return;
+                }
 
-			const action = e.target.getAttribute('data-action');
-			Settings[action].call(e.target);
-		});
-	};
+                const action = e.target.getAttribute('data-action');
+                Settings[action].call(e.target);
+            });
+    };
 
-	Settings.disableAuthn = () => {
-		bootbox.confirm('[[passkeys:user.manage.disableAuthn]]', function (confirm) {
-			if (confirm) {
-				api.del('/plugins/passkeys').then(ajaxify.refresh).catch(alerts.error);
-			}
-		});
-	};
+    Settings.disableAuthn = () => {
+        bootbox.confirm(
+            '[[passkeys:user.manage.disableAuthn]]',
+            function (confirm) {
+                if (confirm) {
+                    api.del('/plugins/passkeys')
+                        .then(ajaxify.refresh)
+                        .catch(alerts.error);
+                }
+            }
+        );
+    };
 
-	Settings.setupAuthn = function () {
-		const self = this;
-		self.classList.add('text-muted');
-		const modal = bootbox.dialog({
-			message: '[[passkeys:authn.modal.content]]',
-			closeButton: false,
-			className: 'text-center',
-		});
-		api.get('/plugins/passkeys/register', {}).then(async (request) => {
-			try {
-				const webauthnJSON = await import('@github/webauthn-json');
-				const response = await webauthnJSON.create({
-					publicKey: request,
-				});
-				modal.modal('hide');
+    Settings.setupAuthn = function () {
+        const self = this;
+        self.classList.add('text-muted');
+        const modal = bootbox.dialog({
+            message: '[[passkeys:authn.modal.content]]',
+            closeButton: false,
+            className: 'text-center',
+        });
+        api.get('/plugins/passkeys/register', {}).then(async (request) => {
+            try {
+                const webauthnJSON = await import('@github/webauthn-json');
+                const response = await webauthnJSON.create({
+                    publicKey: request,
+                });
+                modal.modal('hide');
 
-				api.post('/plugins/passkeys/register', response).then(() => {
-					alerts.success('[[passkeys:authn.success]]');
-					setTimeout(document.location.reload.bind(document.location), 1000);
-				}).catch(alerts.error);
-			} catch (e) {
-				modal.modal('hide');
-				self.classList.remove('disabled');
-				alerts.alert({
-					message: '[[passkeys:authn.error]]',
-					timeout: 2500,
-				});
-			}
-		});
-	};
+                api.post('/plugins/passkeys/register', response)
+                    .then(() => {
+                        alerts.success('[[passkeys:authn.success]]');
+                        setTimeout(
+                            document.location.reload.bind(document.location),
+                            1000
+                        );
+                    })
+                    .catch(alerts.error);
+            } catch (e) {
+                modal.modal('hide');
+                self.classList.remove('disabled');
+                alerts.alert({
+                    message: '[[passkeys:authn.error]]',
+                    timeout: 2500,
+                });
+            }
+        });
+    };
 
-	return Settings;
+    return Settings;
 });
