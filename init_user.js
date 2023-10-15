3f1f5748924b1f593fae58b021b24f618e90a48e
Diogo Correia <diogotc@kth.se>
2023-10-15 17:43:47 +0200

    feat: generate random password for mongodb user

--- init_user.js
+++ init_user.js
@@ -1,3 +1,12 @@
-db = db.getSiblingDB("nodebb");
-db.createUser( { user: "nodebb", pwd: "s8M8ndjq9NIUG76b65", roles: [ "readWrite" ] } );
-db.grantRolesToUser("nodebb",[{ role: "clusterMonitor", db: "admin" }]);
+const crypto = require('crypto');
+const pwd = crypto.randomBytes(32).toString('base64');
+const dbNodebb = db.getSiblingDB("nodebb");
+print("\n\n\n\n\n\n\n\n\n\n\n")
+print("#################################################################");
+print("#                                                               #");
+print(`# nodebb password: ${pwd} #`);
+print("#                                                               #");
+print("#################################################################");
+print("\n\n\n\n\n\n\n\n\n\n\n")
+dbNodebb.createUser({ user: "nodebb", pwd, roles: [ "readWrite" ] });
+dbNodebb.grantRolesToUser("nodebb", [ { role: "clusterMonitor", db: "admin" } ]);
